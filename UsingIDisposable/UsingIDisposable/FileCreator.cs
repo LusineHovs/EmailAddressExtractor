68ca896928984f1c0c76c1199962323c92780658
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-02-09 10:58:07 +0400

    Additions

---
+++ UsingIDisposable/UsingIDisposable/FileCreator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UsingIDisposable
+{
+    class FileCreator :IDisposable
+    {
+        FileStream fs;
+        StreamWriter sw;
+        public FileCreator(string path)
+        {
+            fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            fs.Close();
+            sw = new StreamWriter(path);
+        }
+
+        public void Writing()
+        {
+           // string st = ;
+            
+            sw.Write("Unmaneged codes have already disposed!");
+            sw.Close();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    sw.Dispose();
+                    fs.Dispose();
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+                sw = null;
+                fs = null;
+            }
+        }
+
+       // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+         ~FileCreator()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+
+    }
+}
---
+++ UsingIDisposable/UsingIDisposable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingIDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string path = Path.Combine(desktop, "File1.txt");
+
+            FileCreator fc = new FileCreator(path);
+                fc.Writing();
+
+        }
+    }
+}
